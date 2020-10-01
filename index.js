3b81006a121fc9bc8430eed65f3556653492b378
Guillaume Dieudonné <dieug_@MacBook-Pro-de-Guillaume.local>
2020-10-01 21:53:11 +0200

    fix:code formatting

--- index.js
+++ index.js
@@ -7,23 +7,26 @@ const scrap = require("./services/scrap")
 
 const urls = require("./data/urls.json")
 
-const launchScrapper = async () => {
+const launch = async () => {
   console.log(`Launching Jobs for ${urls.length} propetires`)
 
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
   })
 
-  await Promise.map(urls, url => scrap.run(browser, url), { concurrency: 2 })
+  const launchScrapper = url => scrap.run(browser, url)
+  const options = { concurrency: 2 }
+
+  await Promise.map(urls["V0"], launchScrapper, options)
 
   await browser.close()
 }
 
 const run = async () => {
-  await launchScrapper()
+  await launch()
   new CronJob("*/15 * * * *", async () => {
     console.log(`Job running running 30 MINUTS : ${new Date()}`)
-    await launchScrapper()
+    await launch()
   }).start()
 }
 
